12fbada9a366a1db1228c3d191ef48125b4b58fe
Ikuya Yamada <ikuya@ikuya.net>
2018-03-18 00:07:10 +0900

    add test for MeCabTokenizer

---
+++ tests/utils/tokenizer/test_mecab_tokenizer.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import unittest
+
+from wikipedia2vec.utils.tokenizer.token import Token
+from wikipedia2vec.utils.tokenizer.mecab_tokenizer import MeCabTokenizer
+
+from nose.tools import *
+
+
+class TestMeCabTokenizer(unittest.TestCase):
+    def setUp(self):
+        self._tokenizer = MeCabTokenizer()
+
+    def test_tokenize(self):
+        text = '東京は日本の首都です'
+        tokens = self._tokenizer.tokenize(text)
+
+        ok_(all([isinstance(t, Token) for t in tokens]))
+
+        eq_([(0, 2), (2, 3), (3, 5), (5, 6), (6, 8), (8, 10)], [t.span for t in tokens])
+
+        eq_(['東京', 'は', '日本', 'の', '首都', 'です'], [t.text for t in tokens])
