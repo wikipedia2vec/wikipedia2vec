0754e61abfb3c7f14e8ff09ddc334be83eea0e16
Ikuya Yamada <ikuya@ikuya.net>
2018-05-03 14:15:45 +0900

    use MentionDB in  train command

--- wikipedia2vec/cli.py
+++ wikipedia2vec/cli.py
@@ -99,13 +99,16 @@ def train_embedding_options(func):
 @click.option('--lowercase/--no-lowercase', default=True, help='Whether to lowercase words')
 @click.option('--link-graph/--no-link-graph', default=True, help='Whether to learn from the '
               'Wikipedia link graph')
+@click.option('--link-mentions/--no-link-mentions', default=True, help='Whether to convert entity '
+              'mentions into links')
 @click.option('--tokenizer', default=None, help='The name of a tokenizer used to tokenize a text '
               'into words', type=click.Choice(['regexp', 'icu', 'mecab', 'jieba']))
 @build_dictionary_options
+@build_mention_db_options
 @train_embedding_options
 @common_options
 @click.pass_context
-def train(ctx, out_file, link_graph, tokenizer, **kwargs):
+def train(ctx, out_file, link_graph, link_mentions, tokenizer, **kwargs):
     (out_name, out_ext) = os.path.splitext(os.path.basename(out_file))
     out_path = os.path.dirname(out_file)
 
@@ -125,18 +128,26 @@ def train(ctx, out_file, link_graph, tokenizer, **kwargs):
     logger.info('Starting to build a dictionary...')
     invoke(build_dictionary, tokenizer=tokenizer, out_file=dictionary_file)
 
+    train_embedding_kwargs = {}
     if link_graph:
         logger.info('Starting to build a link graph...')
         link_graph_file = os.path.join(out_path, out_name + '_lg.pkl')
         invoke(build_link_graph, dictionary_file=dictionary_file, out_file=link_graph_file)
 
-        logger.info('Starting to train embeddings...')
-        invoke(train_embedding, dictionary_file=dictionary_file,
-               link_graph=link_graph_file, tokenizer=tokenizer, out_file=out_file)
-    else:
-        logger.info('Starting to train embeddings...')
-        invoke(train_embedding, dictionary_file=dictionary_file, tokenizer=tokenizer,
-               out_file=out_file)
+        train_embedding_kwargs['link_graph'] = link_graph_file
+
+    if link_mentions:
+        logger.info('Starting to build a mention DB...')
+        mention_db_file = os.path.join(out_path, out_name + '_mention.pkl')
+        invoke(build_mention_db, tokenizer=tokenizer, dictionary_file=dictionary_file,
+               out_file=mention_db_file)
+
+        train_embedding_kwargs['mention_db'] = mention_db_file
+
+    logger.info('Starting to train embeddings...')
+
+    invoke(train_embedding, dictionary_file=dictionary_file, tokenizer=tokenizer,
+           out_file=out_file, **train_embedding_kwargs)
 
 
 @cli.command()
