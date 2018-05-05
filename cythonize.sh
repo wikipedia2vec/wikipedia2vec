97f3648e84110333c192b2e9d6b3be0246260ffe
Ikuya Yamada <ikuya@ikuya.net>
2018-05-05 18:35:02 +0900

    revert cythonize.sh

--- cythonize.sh
+++ cythonize.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cythonize -f wikipedia2vec/*.pyx
-cythonize -f wikipedia2vec/utils/*.pyx
-cythonize -f wikipedia2vec/utils/tokenizer/*.pyx
+cython wikipedia2vec/*.pyx
+cython wikipedia2vec/utils/*.pyx
+cython wikipedia2vec/utils/tokenizer/*.pyx
+cython wikipedia2vec/utils/sentence_detector/*.pyx
