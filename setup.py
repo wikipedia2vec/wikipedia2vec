c69c76307ec6b52b396ee9f9c4bbb181840ec7d9
Ikuya Yamada <ikuya@ikuya.net>
2018-05-08 15:59:36 +0900

    bump version

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ class custom_build_ext(build_ext):
 
 setup(
     name='wikipedia2vec',
-    version='0.2',
+    version='0.2.1',
     description='A tool for learning embeddings of words and entities from Wikipedia',
     long_description=long_description,
     author='Studio Ousia',
