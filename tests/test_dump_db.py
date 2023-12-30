cb1ad705f046c3e2aa1b54fe4899060f6d64b72f
Ikuya Yamada <ikuya@ikuya.net>
2023-12-30 16:47:17 +0900

    refactor dump_db & improve the parsing of wikipedia pages

--- tests/test_dump_db.py
+++ tests/test_dump_db.py
@@ -1,147 +1,186 @@
-# -*- coding: utf-8 -*-
-# License: Apache License 2.0
-
-from __future__ import unicode_literals
-import unittest
 import pickle
-import six
+import pkg_resources
+import unittest
 import zlib
+from tempfile import NamedTemporaryFile
 
 from wikipedia2vec import dump_db
-from wikipedia2vec.dump_db import Paragraph, WikiLink
+from wikipedia2vec.dump_db import DumpDB, Paragraph, WikiLink
+from wikipedia2vec.utils.wiki_dump_reader import WikiDumpReader
 from wikipedia2vec.utils.wiki_page import WikiPage
-
-from . import get_dump_db
-
-from nose.tools import *
 
 
 class TestParagraph(unittest.TestCase):
     def test_text_property(self):
-        paragraph = Paragraph('paragraph text', [], False)
-        eq_('paragraph text', paragraph.text)
+        paragraph = Paragraph("paragraph text", [], False)
+        self.assertEqual("paragraph text", paragraph.text)
 
     def test_wiki_link_property(self):
-        wiki_link = WikiLink('Title', 'link text', 0, 3)
-        paragraph = Paragraph('paragraph text', [wiki_link], False)
-        eq_([wiki_link], paragraph.wiki_links)
+        wiki_link = WikiLink("Title", "link text", 0, 3)
+        paragraph = Paragraph("paragraph text", [wiki_link], False)
+        self.assertEqual([wiki_link], paragraph.wiki_links)
 
-    def test_pickle(self):
-        wiki_link = WikiLink('Title', 'link text', 0, 3)
-        paragraph = Paragraph('paragraph text', [wiki_link], False)
-
-        paragraph2 = pickle.loads(pickle.dumps(paragraph))
-        eq_('paragraph text', paragraph2.text)
-        eq_(wiki_link.title, paragraph2.wiki_links[0].title)
-        eq_(wiki_link.text, paragraph2.wiki_links[0].text)
-        eq_(wiki_link.span, paragraph2.wiki_links[0].span)
-
-        eq_(pickle.dumps(paragraph), pickle.dumps(pickle.loads(pickle.dumps(paragraph))))
+    def test_abstract_property(self):
+        paragraph = Paragraph("paragraph text", [], True)
+        self.assertTrue(paragraph.abstract)
+        paragraph = Paragraph("paragraph text", [], False)
+        self.assertFalse(paragraph.abstract)
 
 
 class TestWikiLink(unittest.TestCase):
     def test_title_property(self):
-        wiki_link = WikiLink('WikiTitle', 'link text', 0, 3)
-        eq_('WikiTitle', wiki_link.title)
+        wiki_link = WikiLink("WikiTitle", "link text", 0, 3)
+        self.assertEqual("WikiTitle", wiki_link.title)
 
     def test_text_property(self):
-        wiki_link = WikiLink('WikiTitle', 'link text', 0, 3)
-        eq_('link text', wiki_link.text)
+        wiki_link = WikiLink("WikiTitle", "link text", 0, 3)
+        self.assertEqual("link text", wiki_link.text)
 
     def test_start_property(self):
-        wiki_link = WikiLink('WikiTitle', 'link text', 0, 3)
-        eq_(0, wiki_link.start)
+        wiki_link = WikiLink("WikiTitle", "link text", 0, 3)
+        self.assertEqual(0, wiki_link.start)
 
     def test_end_property(self):
-        wiki_link = WikiLink('WikiTitle', 'link text', 0, 3)
-        eq_(3, wiki_link.end)
+        wiki_link = WikiLink("WikiTitle", "link text", 0, 3)
+        self.assertEqual(3, wiki_link.end)
 
     def test_span_property(self):
-        wiki_link = WikiLink('WikiTitle', 'link text', 0, 3)
-        eq_((0, 3), wiki_link.span)
+        wiki_link = WikiLink("WikiTitle", "link text", 0, 3)
+        self.assertEqual((0, 3), wiki_link.span)
 
-    def test_pickle(self):
-        wiki_link = WikiLink('WikiTitle', 'link text', 0, 3)
-        wiki_link2 = pickle.loads(pickle.dumps(wiki_link))
-        eq_(wiki_link.title, wiki_link2.title)
-        eq_(wiki_link.text, wiki_link2.text)
-        eq_(wiki_link.span, wiki_link2.span)
 
-        eq_(pickle.dumps(wiki_link), pickle.dumps(pickle.loads(pickle.dumps(wiki_link))))
+db = None
+db_file = None
 
 
 class TestDumpDB(unittest.TestCase):
-    def setUp(self):
-        self.db = get_dump_db()
+    @classmethod
+    def setUpClass(cls):
+        global db, db_file
+        dump_file = pkg_resources.resource_filename("tests", "test_data/enwiki-pages-articles-sample.xml.bz2")
+        dump_reader = WikiDumpReader(dump_file)
+        db_file = NamedTemporaryFile()
+
+        DumpDB.build(dump_reader, db_file.name, 1, 1)
+        db = DumpDB(db_file.name)
+
+    @classmethod
+    def tearDownClass(cls):
+        db_file.close()
 
     def test_uuid_property(self):
-        ok_(isinstance(self.db.uuid, six.text_type))
-        eq_(32, len(self.db.uuid))
+        self.assertIsInstance(db.uuid, str)
+        self.assertEqual(32, len(db.uuid))
 
     def test_dump_file_property(self):
-        self.db.dump_file.endswith('enwiki-pages-articles-sample.xml.bz2')
+        self.assertTrue(db.dump_file.endswith("enwiki-pages-articles-sample.xml.bz2"))
 
     def test_language_property(self):
-        eq_('en', self.db.language)
+        self.assertEqual("en", db.language)
 
     def test_page_size(self):
-        eq_(2, self.db.page_size())
+        self.assertEqual(2, db.page_size())
 
     def test_titles_generator(self):
-        eq_(['Accessibility', 'Computer accessibility'], list(self.db.titles()))
+        self.assertEqual(["Accessibility", "Computer accessibility"], list(db.titles()))
 
     def test_redirects_generator(self):
-        eq_([('AccessibleComputing', 'Computer accessibility')], list(self.db.redirects()))
+        self.assertEqual([("AccessibleComputing", "Computer accessibility")], list(db.redirects()))
 
     def test_resolve_redirect(self):
-        eq_('Computer accessibility', self.db.resolve_redirect('AccessibleComputing'))
+        self.assertEqual("Computer accessibility", db.resolve_redirect("AccessibleComputing"))
 
     def test_is_redirect(self):
-        eq_(True, self.db.is_redirect('AccessibleComputing'))
+        self.assertTrue(db.is_redirect("AccessibleComputing"))
 
     def test_is_disambiguation(self):
-        eq_(False, self.db.is_disambiguation('Computer accessibility'))
+        self.assertFalse(db.is_disambiguation("Computer accessibility"))
 
     def test_get_paragraphs(self):
-        paragraphs = self.db.get_paragraphs('Computer accessibility')
+        paragraphs = db.get_paragraphs("Computer accessibility")
         paragraph = paragraphs[0]
 
-        ok_(paragraph.text.replace(' ', '').startswith('Inhuman–computerinteraction'))
+        self.assertTrue(paragraph.text.lstrip().startswith("In human–computer interaction"))
+        self.assertTrue(paragraph.abstract)
         wiki_link = paragraph.wiki_links[0]
-        eq_('Human–computer interaction', wiki_link.title)
-        eq_('human–computer interaction', wiki_link.text)
-        eq_((3, 29), wiki_link.span)
+        self.assertEqual("Human–computer interaction", wiki_link.title)
+        self.assertEqual("human–computer interaction", wiki_link.text)
+        self.assertEqual((5, 31), wiki_link.span)
 
         for paragraph in paragraphs:
-            ok_(isinstance(paragraph, Paragraph))
-
-        for paragraph in paragraphs:
+            self.assertIsInstance(paragraph, Paragraph)
             for wiki_link in paragraph.wiki_links:
-                ok_(isinstance(wiki_link, WikiLink))
-                eq_(paragraph.text[wiki_link.start:wiki_link.end], wiki_link.text)
+                self.assertIsInstance(wiki_link, WikiLink)
+                self.assertEqual(paragraph.text[wiki_link.start : wiki_link.end], wiki_link.text)
 
-    @raises(KeyError)
     def test_get_paragraphs_with_invalid_key(self):
-        self.db.get_paragraphs('foo')
+        self.assertRaises(KeyError, db.get_paragraphs, "foo")
 
-    def test_parse(self):
-        page = WikiPage('Japan', 'en',
-            "'''Japan''' is a [[Sovereign state|sovereign]] [[island country|island nation]] in [[East Asia]]",
-            None
+    def test_parse_page(self):
+        page = WikiPage(
+            title="Japan",
+            language="en",
+            wiki_text="'''Japan''' is a [[Sovereign state|sovereign]] [[island country|island nation]] in <b>[[East Asia]]</b>",
+            redirect=None,
         )
-        ret = dump_db._parse(page, None)
-        eq_('page', ret[0])
-        eq_(b'Japan', ret[1][0])
+        ret = dump_db._parse_page(page, None)
+        self.assertEqual("page", ret[0])
+        self.assertEqual(b"Japan", ret[1][0])
         paragraph = pickle.loads(zlib.decompress(ret[1][1]))[0][0]
-        eq_('Japan is a sovereign island nation in East Asia', paragraph[0])
-        eq_([('Sovereign state', 'sovereign', 11, 20),
-             ('Island country', 'island nation', 21, 34),
-             ('East Asia', 'East Asia', 38, 47)],
-            paragraph[1])
+        self.assertEqual("Japan is a sovereign island nation in East Asia", paragraph[0])
+        self.assertEqual(
+            [
+                ("Sovereign state", "sovereign", 11, 20),
+                ("Island country", "island nation", 21, 34),
+                ("East Asia", "East Asia", 38, 47),
+            ],
+            paragraph[1],
+        )
 
-    def test_parse_redirect(self):
-        page = WikiPage('日本', 'en', '#REDIRECT [[Japan]]', 'Japan')
-        ret = dump_db._parse(page, None)
-        eq_('redirect', ret[0])
-        eq_('日本'.encode('utf-8'), ret[1][0])
-        eq_(b'Japan', ret[1][1])
+    def test_parse_page_with_category_link(self):
+        page = WikiPage(
+            title="Japan",
+            language="en",
+            wiki_text="[[Category:Asia]]",
+            redirect=None,
+        )
+        ret = dump_db._parse_page(page, None)
+        paragraph = pickle.loads(zlib.decompress(ret[1][1]))[0][0]
+        # Category prefix should be removed
+        self.assertEqual("Asia", paragraph[0])
+        self.assertEqual([("Category:Asia", "Asia", 0, 4)], paragraph[1])
+
+        page = WikiPage(
+            title="Japan",
+            language="en",
+            wiki_text="[[Category:Asia|Asian countries]]",
+            redirect=None,
+        )
+        ret = dump_db._parse_page(page, None)
+        paragraph = pickle.loads(zlib.decompress(ret[1][1]))[0][0]
+        self.assertEqual("Asian countries", paragraph[0])
+        self.assertEqual([("Category:Asia", "Asian countries", 0, 15)], paragraph[1])
+
+    def test_parse_page_with_media_link(self):
+        page = WikiPage(
+            title="Japan",
+            language="en",
+            wiki_text="[[File:japan.csv]] / [[Image:japan.png|Image]]",
+            redirect=None,
+        )
+        ret = dump_db._parse_page(page, None)
+        paragraph = pickle.loads(zlib.decompress(ret[1][1]))[0][0]
+        # Media links should be removed
+        self.assertEqual(" / ", paragraph[0])
+        self.assertFalse(paragraph[1])
+
+    def test_parse_page_redirect(self):
+        page = WikiPage(title="日本", language="en", wiki_text="#REDIRECT [[Japan]]", redirect="Japan")
+        ret = dump_db._parse_page(page, None)
+        self.assertEqual("redirect", ret[0])
+        self.assertEqual("日本".encode("utf-8"), ret[1][0])
+        self.assertEqual(b"Japan", ret[1][1])
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ wikipedia2vec/dump_db.py
@@ -0,0 +1,300 @@
+import logging
+import pickle
+import re
+import zlib
+from contextlib import closing
+from functools import partial
+from multiprocessing.pool import Pool
+from typing import Callable, Dict, Iterator, List, NamedTuple, Optional, Tuple
+from uuid import uuid1
+
+import cython
+import lmdb
+import mwparserfromhell
+
+from .utils.utils import CATEGORY_ALIASES, MEDIA_ALIASES, normalize_title
+from .utils.wiki_dump_reader import WikiDumpReader
+from .utils.wiki_page import WikiPage
+
+logger = logging.getLogger(__name__)
+
+
+@cython.cclass
+class WikiLink:
+    title = cython.declare(str, visibility="public")
+    text = cython.declare(str, visibility="public")
+    start = cython.declare(cython.int, visibility="public")
+    end = cython.declare(cython.int, visibility="public")
+
+    def __init__(self, title: str, text: str, start: int, end: int):
+        self.title = title
+        self.text = text
+        self.start = start
+        self.end = end
+
+    @property
+    def span(self) -> Tuple[int, int]:
+        return (self.start, self.end)
+
+    def __reduce__(self):
+        return (self.__class__, (self.title, self.text, self.start, self.end))
+
+    def __repr__(self):
+        return f"<WikiLink {self.text}->{self.title}>"
+
+
+@cython.cclass
+class Paragraph:
+    text = cython.declare(str, visibility="public")
+    wiki_links = cython.declare(List[WikiLink], visibility="public")
+    abstract = cython.declare(cython.bint, visibility="public")
+
+    def __init__(self, text: str, wiki_links: List[WikiLink], abstract: bool):
+        self.text = text
+        self.wiki_links = wiki_links
+        self.abstract = abstract
+
+    def __reduce__(self):
+        return (self.__class__, (self.text, self.wiki_links, self.abstract))
+
+    def __repr__(self):
+        return f"<Paragraph {self.text[:50] + '...'}>"
+
+
+class DumpDB:
+    def __init__(self, db_file: str):
+        self._db_file = db_file
+
+        self._env = lmdb.open(db_file, readonly=True, subdir=False, lock=False, max_dbs=3)
+        self._meta_db = self._env.open_db(b"__meta__")
+        self._page_db = self._env.open_db(b"__page__")
+        self._redirect_db = self._env.open_db(b"__redirect__")
+
+    def __reduce__(self):
+        return (self.__class__, (self._db_file,))
+
+    @property
+    def uuid(self) -> str:
+        with self._env.begin(db=self._meta_db) as txn:
+            return txn.get(b"id").decode("utf-8")
+
+    @property
+    def dump_file(self) -> str:
+        with self._env.begin(db=self._meta_db) as txn:
+            return txn.get(b"dump_file").decode("utf-8")
+
+    @property
+    def language(self) -> str:
+        with self._env.begin(db=self._meta_db) as txn:
+            return txn.get(b"language").decode("utf-8")
+
+    def page_size(self) -> int:
+        with self._env.begin(db=self._page_db) as txn:
+            return txn.stat()["entries"]
+
+    def titles(self) -> Iterator[str]:
+        with self._env.begin(db=self._page_db) as txn:
+            cur = txn.cursor()
+            for key in cur.iternext(values=False):
+                yield key.decode("utf-8")
+
+    def redirects(self) -> Iterator[Tuple[str, str]]:
+        with self._env.begin(db=self._redirect_db) as txn:
+            cur = txn.cursor()
+            for key, value in iter(cur):
+                yield (key.decode("utf-8"), value.decode("utf-8"))
+
+    def resolve_redirect(self, title: str) -> str:
+        with self._env.begin(db=self._redirect_db) as txn:
+            value = txn.get(title.encode("utf-8"))
+            if value:
+                return value.decode("utf-8")
+            else:
+                return title
+
+    def is_redirect(self, title: str) -> bool:
+        with self._env.begin(db=self._redirect_db) as txn:
+            value = txn.get(title.encode("utf-8"))
+
+        return bool(value)
+
+    def is_disambiguation(self, title: str) -> bool:
+        with self._env.begin(db=self._page_db) as txn:
+            value = txn.get(title.encode("utf-8"))
+
+        if not value:
+            return False
+
+        return pickle.loads(zlib.decompress(value))[1]
+
+    def get_paragraphs(self, key: str) -> List[Paragraph]:
+        with self._env.begin(db=self._page_db) as txn:
+            value = txn.get(key.encode("utf-8"))
+            if not value:
+                raise KeyError(key)
+
+        return self._deserialize_paragraphs(value)
+
+    def _deserialize_paragraphs(self, value: bytes) -> List[Paragraph]:
+        ret = []
+        for obj in pickle.loads(zlib.decompress(value))[0]:
+            wiki_links = [WikiLink(*args) for args in obj[1]]
+            ret.append(Paragraph(obj[0], wiki_links, obj[2]))
+
+        return ret
+
+    @staticmethod
+    def build(
+        dump_reader: WikiDumpReader,
+        out_file: str,
+        pool_size: int,
+        chunk_size: int,
+        preprocess_func: Optional[Callable[[str], str]] = None,
+        init_map_size: int = 500000000,
+        buffer_size: int = 3000,
+    ):
+        with closing(lmdb.open(out_file, subdir=False, map_async=True, map_size=init_map_size, max_dbs=3)) as env:
+            map_size = [init_map_size]
+            meta_db = env.open_db(b"__meta__")
+            with env.begin(db=meta_db, write=True) as txn:
+                txn.put(b"id", uuid1().hex.encode("utf-8"))
+                txn.put(b"dump_file", dump_reader.dump_file.encode("utf-8"))
+                txn.put(b"language", dump_reader.language.encode("utf-8"))
+
+            page_db = env.open_db(b"__page__")
+            redirect_db = env.open_db(b"__redirect__")
+
+            def write_db(db, data):
+                try:
+                    with env.begin(db=db, write=True) as txn:
+                        txn.cursor().putmulti(data)
+
+                except lmdb.MapFullError:
+                    map_size[0] *= 2
+                    env.set_mapsize(map_size[0])
+
+                    write_db(db, data)
+
+            with closing(Pool(pool_size)) as pool:
+                page_buf = []
+                redirect_buf = []
+                f = partial(_parse_page, preprocess_func=preprocess_func)
+                for ret in pool.imap_unordered(f, dump_reader, chunksize=chunk_size):
+                    if ret:
+                        if ret[0] == "page":
+                            page_buf.append(ret[1])
+                        else:  # redirect
+                            redirect_buf.append(ret[1])
+
+                    if len(page_buf) == buffer_size:
+                        write_db(page_db, page_buf)
+                        page_buf = []
+
+                    if len(redirect_buf) == buffer_size:
+                        write_db(redirect_db, redirect_buf)
+                        redirect_buf = []
+
+                if page_buf:
+                    write_db(page_db, page_buf)
+
+                if redirect_buf:
+                    write_db(redirect_db, redirect_buf)
+
+
+_media_re_cache: Dict[str, re.Pattern] = {}
+_category_re_cache: Dict[str, re.Pattern] = {}
+
+
+def _parse_page(page: WikiPage, preprocess_func: Optional[Callable[[str], str]]):
+    if page.is_redirect:
+        return ("redirect", (page.title.encode("utf-8"), page.redirect.encode("utf-8")))
+
+    # remove style tags to reduce parsing errors
+    wiki_text = re.sub("'''*", "", page.wiki_text)
+    try:
+        parsed_wiki_text = mwparserfromhell.parse(wiki_text)
+    except Exception:
+        logger.warn(f"Failed to parse wiki text: {page.title}")
+        return None
+
+    media_re = _media_re_cache.get(page.language)
+    if media_re is None:
+        media_prefixes = "|".join(["File", "Image", "Media"] + MEDIA_ALIASES.get(page.language, []))
+        media_re = re.compile(f"^(?:{media_prefixes}):", flags=re.IGNORECASE)
+        _media_re_cache[page.language] = media_re
+
+    category_re = _category_re_cache.get(page.language)
+    if category_re is None:
+        category_prefixes = "|".join(["Category"] + CATEGORY_ALIASES.get(page.language, []))
+        category_re = re.compile(f"^(?:{category_prefixes}):", flags=re.IGNORECASE)
+        _category_re_cache[page.language] = category_re
+
+    class WikiLinkTuple(NamedTuple):
+        text: str
+        title: str
+
+    def extract_text(node: mwparserfromhell.nodes.Node) -> str:
+        text = node.__strip__(normalize=True, collapse=True, keep_template_params=False)
+        if text is None:
+            return ""
+
+        text = str(text)
+        if preprocess_func is not None:
+            text = preprocess_func(text)
+        return text
+
+    def parse_node(node: mwparserfromhell.nodes.Node) -> list[str | WikiLinkTuple]:
+        if isinstance(node, mwparserfromhell.nodes.Wikilink):
+            if media_re.match(str(node.title)):  # ignore the link if it points to a media file
+                return []
+
+            text = extract_text(node)
+            if category_re.match(str(node.title)):
+                text = re.sub(category_re, "", text)  # remove the category prefix
+
+            title = node.title.strip_code().strip(" ")
+            if title.startswith(":"):
+                title = title[1:]
+            if not title:
+                return [text]
+
+            title = normalize_title(title)
+            return [WikiLinkTuple(text=text, title=title)]
+
+        elif isinstance(node, mwparserfromhell.nodes.Tag):
+            # ignore references and tables
+            if str(node.tag) in {"ref", "table"}:
+                return []
+            elif not mwparserfromhell.definitions.is_visible(node.tag):
+                return []
+
+            parsed_child_nodes = []
+            for child_node in node.contents.ifilter(recursive=False):
+                parsed_child_nodes += parse_node(child_node)
+            return parsed_child_nodes
+
+        text = extract_text(node)
+        return [text]
+
+    paragraphs = []
+    is_abstract = True
+    for section in parsed_wiki_text.get_sections(flat=True, include_lead=True, include_headings=True):
+        cur_text = ""
+        cur_links = []
+        for node in section.ifilter(recursive=False):
+            for str_or_wiki_link in parse_node(node):
+                if isinstance(str_or_wiki_link, WikiLinkTuple):
+                    start = len(cur_text)
+                    end = start + len(str_or_wiki_link.text)
+                    cur_links.append((str_or_wiki_link.title, str_or_wiki_link.text, start, end))
+                    cur_text += str_or_wiki_link.text
+                else:  # str
+                    cur_text += str_or_wiki_link
+
+        if cur_text:
+            paragraphs.append((cur_text, cur_links, is_abstract))
+            is_abstract = False
+
+    ret = (tuple(paragraphs), page.is_disambiguation)
+
+    return ("page", ((page.title.encode("utf-8"), zlib.compress(pickle.dumps(ret, protocol=-1)))))
