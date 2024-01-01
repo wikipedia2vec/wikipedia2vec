872d78fd70390d2938f4f9aab4adcfcef11b540b
Ikuya Yamada <ikuya@ikuya.net>
2024-01-01 16:34:08 +0900

    refactor dictionary.py

--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -1,222 +1,272 @@
-# -*- coding: utf-8 -*-
-# License: Apache License 2.0
-
-from __future__ import unicode_literals
-import numpy as np
-import pickle
-import six
+import pkg_resources
 import unittest
 from tempfile import NamedTemporaryFile
+from unittest import mock
+
+import numpy as np
 
 from wikipedia2vec.dictionary import Dictionary, Item, Word, Entity
+from wikipedia2vec.dump_db import DumpDB
 from wikipedia2vec.utils.tokenizer import get_tokenizer
-
-from . import get_dump_db
-
-from nose.tools import *
+from wikipedia2vec.utils.wiki_dump_reader import WikiDumpReader
 
 
 class TestWord(unittest.TestCase):
     def test_text_property(self):
-        word = Word('text', 1000, 100, 10)
-        eq_(word.text, 'text')
+        word = Word("text", 1000, 100, 10)
+        self.assertEqual(word.text, "text")
 
     def test_index_property(self):
-        word = Word('text', 1000, 100, 10)
-        eq_(word.index, 1000)
+        word = Word("text", 1000, 100, 10)
+        self.assertEqual(word.index, 1000)
 
     def test_count_property(self):
-        word = Word('text', 1000, 100, 10)
-        eq_(word.count, 100)
+        word = Word("text", 1000, 100, 10)
+        self.assertEqual(word.count, 100)
 
     def test_doc_count_property(self):
-        word = Word('text', 1000, 100, 10)
-        eq_(word.doc_count, 10)
-
-    def test_pickle(self):
-        word = Word('text', 1000, 100, 10)
-        word2 = pickle.loads(pickle.dumps(word))
-        eq_(word2.text, word.text)
-        eq_(word2.index, word.index)
-        eq_(word2.count, word.count)
-        eq_(word2.doc_count, word.doc_count)
+        word = Word("text", 1000, 100, 10)
+        self.assertEqual(word.doc_count, 10)
 
 
 class TestEntity(unittest.TestCase):
     def test_title_property(self):
-        entity = Entity('Title', 1000, 100, 10)
-        eq_(entity.title, 'Title')
+        entity = Entity("Title", 1000, 100, 10)
+        self.assertEqual(entity.title, "Title")
 
     def test_index_property(self):
-        entity = Entity('Title', 1000, 100, 10)
-        eq_(entity.index, 1000)
+        entity = Entity("Title", 1000, 100, 10)
+        self.assertEqual(entity.index, 1000)
 
     def test_count_property(self):
-        entity = Entity('Title', 1000, 100, 10)
-        eq_(entity.count, 100)
+        entity = Entity("Title", 1000, 100, 10)
+        self.assertEqual(entity.count, 100)
 
     def test_doc_count_property(self):
-        entity = Entity('Title', 1000, 100, 10)
-        eq_(entity.doc_count, 10)
+        entity = Entity("Title", 1000, 100, 10)
+        self.assertEqual(entity.doc_count, 10)
 
-    def test_pickle(self):
-        entity = Entity('Title', 1000, 100, 10)
-        entity2 = pickle.loads(pickle.dumps(entity))
-        eq_(entity2.title, entity.title)
-        eq_(entity2.index, entity.index)
-        eq_(entity2.count, entity.count)
-        eq_(entity2.doc_count, entity.doc_count)
+
+db = None
+db_file = None
+dictionary = None
 
 
 class TestDictionary(unittest.TestCase):
-    def setUp(self):
-        tokenizer = get_tokenizer('regexp')
-        self.dictionary = Dictionary.build(get_dump_db(), tokenizer=tokenizer, lowercase=True,
-                                           min_word_count=2, min_entity_count=1,
-                                           min_paragraph_len=5, category=True, disambi=False,
-                                           pool_size=1, chunk_size=1, progressbar=False)
+    @classmethod
+    def setUpClass(cls):
+        global db, db_file, tokenizer, dictionary
+        dump_file = pkg_resources.resource_filename("tests", "test_data/enwiki-pages-articles-sample.xml.bz2")
+        dump_reader = WikiDumpReader(dump_file)
+        db_file = NamedTemporaryFile()
+
+        DumpDB.build(dump_reader, db_file.name, 1, 1)
+        db = DumpDB(db_file.name)
+
+        tokenizer = get_tokenizer("regexp")
+        dictionary = Dictionary.build(
+            db,
+            tokenizer=tokenizer,
+            lowercase=True,
+            min_word_count=2,
+            min_entity_count=1,
+            min_paragraph_len=5,
+            category=True,
+            disambi=True,
+            pool_size=1,
+            chunk_size=1,
+            progressbar=False,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        db_file.close()
 
     def test_uuid_property(self):
-        ok_(isinstance(self.dictionary.uuid, six.text_type))
-        eq_(32, len(self.dictionary.uuid))
+        self.assertIsInstance(dictionary.uuid, str)
+        self.assertEqual(32, len(dictionary.uuid))
 
     def test_language_property(self):
-        eq_('en', self.dictionary.language)
+        self.assertEqual("en", dictionary.language)
 
     def test_lowercase_property(self):
-        eq_(True, self.dictionary.lowercase)
+        self.assertEqual(True, dictionary.lowercase)
 
     def test_build_params_property(self):
-        build_params = self.dictionary.build_params
-        eq_(build_params['dump_db'], get_dump_db().uuid)
-        ok_(build_params['dump_file'].endswith('enwiki-pages-articles-sample.xml.bz2'))
-        eq_(2, build_params['min_word_count'])
-        eq_(1, build_params['min_entity_count'])
-        ok_(isinstance(build_params['build_time'], float))
-        ok_(build_params['build_time'] > 0)
+        build_params = dictionary.build_params
+        self.assertEqual(build_params["dump_db"], db.uuid)
+        self.assertTrue(build_params["dump_file"].endswith("enwiki-pages-articles-sample.xml.bz2"))
+        self.assertEqual(2, build_params["min_word_count"])
+        self.assertEqual(1, build_params["min_entity_count"])
+        self.assertIsInstance(build_params["build_time"], float)
+        self.assertGreater(build_params["build_time"], 0)
 
     def test_min_paragraph_len_property(self):
-        eq_(5, self.dictionary.min_paragraph_len)
+        self.assertEqual(5, dictionary.min_paragraph_len)
 
     def test_entity_offset_property(self):
-        eq_(898, self.dictionary.entity_offset)
+        self.assertEqual(910, dictionary.entity_offset)
 
     def test_word_size_property(self):
-        eq_(898, self.dictionary.word_size)
+        self.assertEqual(910, dictionary.word_size)
 
     def test_entity_size_property(self):
-        eq_(242, self.dictionary.entity_size)
+        self.assertEqual(232, dictionary.entity_size)
 
     def test_len(self):
-        eq_(1140, len(self.dictionary))
+        self.assertEqual(1142, len(dictionary))
 
     def test_iterator(self):
-        items = list(self.dictionary)
-        eq_(len(self.dictionary), len(items))
-        ok_(all(isinstance(item, Item) for item in items))
+        items = list(dictionary)
+        self.assertEqual(len(dictionary), len(items))
+        for item in items:
+            self.assertIsInstance(item, Item)
 
     def test_words_iterator(self):
-        words = list(self.dictionary.words())
-        eq_(self.dictionary.word_size, len(words))
-        ok_(all(isinstance(word, Word) for word in words))
+        words = list(dictionary.words())
+        self.assertEqual(dictionary.word_size, len(words))
+        for word in words:
+            self.assertIsInstance(word, Word)
 
     def test_entities_iterator(self):
-        entities = list(self.dictionary.entities())
-        eq_(self.dictionary.entity_size, len(entities))
-        ok_(all(isinstance(entity, Entity) for entity in entities))
+        entities = list(dictionary.entities())
+        self.assertEqual(dictionary.entity_size, len(entities))
+        for entity in entities:
+            self.assertIsInstance(entity, Entity)
 
     def test_get_word(self):
-        word = self.dictionary.get_word('the')
-        ok_(isinstance(word, Word))
-        eq_('the', word.text)
-        eq_(193, word.index)
-        eq_(429, word.count)
-        eq_(2, word.doc_count)
+        word = dictionary.get_word("the")
+        self.assertIsInstance(word, Word)
+        self.assertEqual("the", word.text)
+        self.assertEqual(201, word.index)
+        self.assertEqual(424, word.count)
+        self.assertEqual(2, word.doc_count)
 
     def test_get_word_not_exist(self):
-        eq_(None, self.dictionary.get_word('foobar'))
+        self.assertEqual(None, dictionary.get_word("foobar"))
 
     def test_get_word_index(self):
-        eq_(193, self.dictionary.get_word_index('the'))
+        self.assertEqual(201, dictionary.get_word_index("the"))
 
     def test_get_word_index_not_exist(self):
-        eq_(-1, self.dictionary.get_word_index('foobar'))
+        self.assertEqual(-1, dictionary.get_word_index("foobar"))
 
     def test_get_entity(self):
-        entity = self.dictionary.get_entity('Computer system')
-        ok_(isinstance(entity, Entity))
-        eq_(1132, entity.index)
-        eq_(1, entity.count)
-        eq_(1, entity.doc_count)
+        entity = dictionary.get_entity("Computer system")
+        self.assertIsInstance(entity, Entity)
+        self.assertEqual(1134, entity.index)
+        self.assertEqual(1, entity.count)
+        self.assertEqual(1, entity.doc_count)
 
     def test_get_entity_redirect(self):
-        eq_('Computer accessibility', self.dictionary.get_entity('AccessibleComputing').title)
-        eq_(None, self.dictionary.get_entity('AccessibleComputing', resolve_redirect=False))
+        self.assertEqual("Computer accessibility", dictionary.get_entity("AccessibleComputing").title)
+        self.assertIsNone(dictionary.get_entity("AccessibleComputing", resolve_redirect=False))
 
     def test_get_entity_not_exist(self):
-        eq_(None, self.dictionary.get_entity('Foo'))
+        self.assertIsNone(dictionary.get_entity("Foo"))
 
     def test_get_entity_index(self):
-        eq_(1132, self.dictionary.get_entity_index('Computer system'))
+        self.assertEqual(1134, dictionary.get_entity_index("Computer system"))
 
     def test_get_entity_index_not_exist(self):
-        eq_(-1, self.dictionary.get_entity_index('Foo'))
+        self.assertEqual(-1, dictionary.get_entity_index("Foo"))
 
     def test_get_item_by_index(self):
-        item = self.dictionary.get_item_by_index(193)
-        ok_(isinstance(item, Word))
-        eq_('the', item.text)
-        eq_(193, item.index)
-        eq_(429, item.count)
-        eq_(2, item.doc_count)
+        item = dictionary.get_item_by_index(201)
+        self.assertIsInstance(item, Word)
+        self.assertEqual("the", item.text)
+        self.assertEqual(201, item.index)
+        self.assertEqual(424, item.count)
+        self.assertEqual(2, item.doc_count)
 
-        item2 = self.dictionary.get_item_by_index(1132)
-        ok_(isinstance(item2, Entity))
-        eq_('Computer system', item2.title)
-        eq_(1132, item2.index)
-        eq_(1, item2.count)
-        eq_(1, item2.doc_count)
+        item2 = dictionary.get_item_by_index(1134)
+        self.assertIsInstance(item2, Entity)
+        self.assertEqual("Computer system", item2.title)
+        self.assertEqual(1134, item2.index)
+        self.assertEqual(1, item2.count)
+        self.assertEqual(1, item2.doc_count)
 
-    @raises(KeyError)
     def test_get_item_by_index_not_exist(self):
-        self.dictionary.get_item_by_index(100000)
+        self.assertRaises(KeyError, dictionary.get_item_by_index, 100000)
 
     def test_get_word_by_index(self):
-        word = self.dictionary.get_word_by_index(193)
-        ok_(isinstance(word, Word))
-        eq_('the', word.text)
-        eq_(193, word.index)
-        eq_(429, word.count)
-        eq_(2, word.doc_count)
+        word = dictionary.get_word_by_index(201)
+        self.assertIsInstance(word, Word)
+        self.assertEqual("the", word.text)
+        self.assertEqual(201, word.index)
+        self.assertEqual(424, word.count)
+        self.assertEqual(2, word.doc_count)
 
-    @raises(KeyError)
     def test_get_word_by_index_not_exist(self):
-        self.dictionary.get_word_by_index(899)
+        self.assertRaises(KeyError, dictionary.get_word_by_index, 1000)
 
     def test_get_entity_by_index(self):
-        entity = self.dictionary.get_entity_by_index(1132)
-        ok_(isinstance(entity, Entity))
-        eq_('Computer system', entity.title)
-        eq_(1132, entity.index)
-        eq_(1, entity.count)
-        eq_(1, entity.doc_count)
+        entity = dictionary.get_entity_by_index(1134)
+        self.assertIsInstance(entity, Entity)
+        self.assertEqual("Computer system", entity.title)
+        self.assertEqual(1134, entity.index)
+        self.assertEqual(1, entity.count)
+        self.assertEqual(1, entity.doc_count)
 
-    @raises(KeyError)
     def test_get_entity_by_index_not_exist(self):
-        self.dictionary.get_entity_by_index(0)
+        self.assertRaises(KeyError, dictionary.get_entity_by_index, 0)
+
+    def test_build_without_category(self):
+        dictionary_no_category = Dictionary.build(
+            db,
+            tokenizer=tokenizer,
+            lowercase=True,
+            min_word_count=2,
+            min_entity_count=1,
+            min_paragraph_len=5,
+            category=False,
+            disambi=True,
+            pool_size=1,
+            chunk_size=1,
+            progressbar=False,
+        )
+        for entity in dictionary_no_category.entities():
+            self.assertFalse(entity.title.startswith("Category:"))
+
+    def test_build_without_disambi(self):
+        # treat "Computer accessibility" as a disambiguation page
+        with mock.patch.object(
+            DumpDB,
+            "is_disambiguation",
+            new_callable=lambda: lambda self, title: bool(title == "Computer accessibility"),
+        ):
+            dictionary_no_disambi = Dictionary.build(
+                db,
+                tokenizer=tokenizer,
+                lowercase=True,
+                min_word_count=2,
+                min_entity_count=1,
+                min_paragraph_len=5,
+                category=True,
+                disambi=False,
+                pool_size=1,
+                chunk_size=1,
+                progressbar=False,
+            )
+        self.assertIsNone(dictionary_no_disambi.get_entity("Computer accessibility"))
+        self.assertIsNotNone(dictionary_no_disambi.get_entity("Computer system"))
 
     def test_save_load(self):
         def validate(obj):
-            s1 = self.dictionary.serialize()
+            s1 = dictionary.serialize()
             s2 = obj.serialize()
             for key in s1.keys():
                 if isinstance(s1[key], np.ndarray):
                     np.array_equal(s1[key], s2[key])
                 else:
-                    eq_(s1[key], s2[key])
+                    self.assertEqual(s1[key], s2[key])
 
-        validate(Dictionary.load(self.dictionary.serialize()))
+        validate(Dictionary.load(dictionary.serialize()))
 
         with NamedTemporaryFile() as f:
-            self.dictionary.save(f.name)
+            dictionary.save(f.name)
             validate(Dictionary.load(f.name))
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ wikipedia2vec/dictionary.py
@@ -0,0 +1,370 @@
+import logging
+import multiprocessing
+import time
+from collections import Counter
+from contextlib import closing
+from functools import partial
+from itertools import chain
+from multiprocessing.pool import Pool
+from typing import Any, Iterator, Optional, Union
+from uuid import uuid1
+
+import cython
+import joblib
+import numpy as np
+from marisa_trie import Trie, RecordTrie
+from tqdm import tqdm
+
+from .dump_db import DumpDB
+from .utils.tokenizer.base_tokenizer import BaseTokenizer
+from .utils.utils import is_category_title
+
+logger = logging.getLogger(__name__)
+
+
+@cython.cclass
+class Item:
+    index = cython.declare(cython.int, visibility="public")
+    count = cython.declare(cython.int, visibility="public")
+    doc_count = cython.declare(cython.int, visibility="public")
+
+    def __init__(self, index: int, count: int, doc_count: int):
+        self.index = index
+        self.count = count
+        self.doc_count = doc_count
+
+
+@cython.cclass
+class Word(Item):
+    text = cython.declare(str, visibility="public")
+
+    def __init__(self, text: str, index: int, count: int, doc_count: int):
+        super().__init__(index, count, doc_count)
+        self.text = text
+
+    def __reduce__(self):
+        return (self.__class__, (self.text, self.index, self.count, self.doc_count))
+
+    def __repr__(self):
+        return f"<Word {self.text}>"
+
+
+@cython.cclass
+class Entity(Item):
+    title = cython.declare(str, visibility="public")
+
+    def __init__(self, title: str, index: int, count: int, doc_count: int):
+        super().__init__(index, count, doc_count)
+        self.title = title
+
+    def __reduce__(self):
+        return (self.__class__, (self.title, self.index, self.count, self.doc_count))
+
+    def __repr__(self):
+        return f"<Entity {self.title}>"
+
+
+class Dictionary:
+    def __init__(
+        self,
+        word_dict: Trie,
+        entity_dict: Trie,
+        redirect_dict: RecordTrie,
+        word_stats: np.ndarray,
+        entity_stats: np.ndarray,
+        language: str,
+        lowercase: bool,
+        build_params: dict,
+        min_paragraph_len: int = 0,
+        uuid: str = "",
+    ):
+        self._word_dict = word_dict
+        self._entity_dict = entity_dict
+        self._redirect_dict = redirect_dict
+
+        # Limit word_stats size in order to handle the case where existing pretrained embedding is larger than it should be
+        self._word_stats = word_stats[: len(self._word_dict)]
+        self._entity_stats = entity_stats[: len(self._entity_dict)]
+
+        self.min_paragraph_len = min_paragraph_len
+        self.uuid = uuid
+        self.language = language
+        self.lowercase = lowercase
+        self.build_params = build_params
+
+        self._entity_offset = len(self._word_dict)
+
+    @property
+    def entity_offset(self) -> int:
+        return self._entity_offset
+
+    @property
+    def word_size(self) -> int:
+        return len(self._word_dict)
+
+    @property
+    def entity_size(self) -> int:
+        return len(self._entity_dict)
+
+    def __len__(self) -> int:
+        return len(self._word_dict) + len(self._entity_dict)
+
+    def __iter__(self) -> Iterator[Item]:
+        return chain(self.words(), self.entities())
+
+    def words(self) -> Iterator[Word]:
+        for word, index in self._word_dict.items():
+            yield Word(word, index, *self._word_stats[index].tolist())
+
+    def entities(self) -> Iterator[Entity]:
+        for title, index in self._entity_dict.items():
+            yield Entity(title, index + self._entity_offset, *self._entity_stats[index].tolist())
+
+    def get_word(self, word: str, default: Any = None) -> Any:
+        index = self.get_word_index(word)
+        if index == -1:
+            return default
+        else:
+            return Word(word, index, *self._word_stats[index].tolist())
+
+    def get_word_index(self, word: str) -> int:
+        try:
+            return self._word_dict[word]
+        except KeyError:
+            return -1
+
+    def get_entity(self, title: str, resolve_redirect: bool = True, default: Any = None) -> Any:
+        index = self.get_entity_index(title, resolve_redirect=resolve_redirect)
+        if index == -1:
+            return default
+        else:
+            dict_index = index - self._entity_offset
+            title = self._entity_dict.restore_key(dict_index)
+            return Entity(title, index, *self._entity_stats[dict_index].tolist())
+
+    def get_entity_index(self, title: str, resolve_redirect: bool = True) -> int:
+        if resolve_redirect:
+            try:
+                index = self._redirect_dict[title][0][0]
+                return index + self._entity_offset
+            except KeyError:
+                pass
+        try:
+            index = self._entity_dict[title]
+            return index + self._entity_offset
+        except KeyError:
+            return -1
+
+    def get_item_by_index(self, index: int) -> Item:
+        if index < self._entity_offset:
+            return self.get_word_by_index(index)
+        else:
+            return self.get_entity_by_index(index)
+
+    def get_word_by_index(self, index: int) -> Word:
+        word = self._word_dict.restore_key(index)
+        return Word(word, index, *self._word_stats[index].tolist())
+
+    def get_entity_by_index(self, index: int) -> Entity:
+        dict_index = index - self._entity_offset
+        title = self._entity_dict.restore_key(dict_index)
+        return Entity(title, index, *self._entity_stats[dict_index].tolist())
+
+    @staticmethod
+    def build(
+        dump_db: DumpDB,
+        tokenizer: BaseTokenizer,
+        lowercase: bool,
+        min_word_count: int,
+        min_entity_count: int,
+        min_paragraph_len: int,
+        category: bool,
+        disambi: bool,
+        pool_size: int,
+        chunk_size: int,
+        progressbar: bool = True,
+    ) -> "Dictionary":
+        start_time = time.time()
+
+        logger.info("Step 1/2: Processing Wikipedia pages...")
+
+        word_counter = Counter()
+        word_doc_counter = Counter()
+        entity_counter = Counter()
+        entity_doc_counter = Counter()
+
+        with closing(Pool(pool_size, initializer=init_worker, initargs=(dump_db, tokenizer))) as pool:
+            with tqdm(total=dump_db.page_size(), mininterval=0.5, disable=not progressbar) as bar:
+                f = partial(_process_page, lowercase=lowercase, min_paragraph_len=min_paragraph_len)
+                for word_cnt, entity_cnt in pool.imap_unordered(f, dump_db.titles(), chunksize=chunk_size):
+                    for word, count in word_cnt.items():
+                        word_counter[word] += count
+                        word_doc_counter[word] += 1
+
+                    for title, count in entity_cnt.items():
+                        if "#" in title:
+                            continue
+                        entity_counter[title] += count
+                        entity_doc_counter[title] += 1
+
+                    bar.update(1)
+
+        logger.info("Step 2/2: Processing Wikipedia redirects...")
+
+        for title, dest_title in dump_db.redirects():
+            entity_counter[dest_title] += entity_counter[title]
+            del entity_counter[title]
+
+            entity_doc_counter[dest_title] += entity_doc_counter[title]
+            del entity_doc_counter[title]
+
+        word_dict = Trie([w for (w, c) in word_counter.items() if c >= min_word_count])
+        word_stats = np.zeros((len(word_dict), 2), dtype=np.int32)
+        for word, index in word_dict.items():
+            word_stats[index][0] = word_counter[word]
+            word_stats[index][1] = word_doc_counter[word]
+
+        del word_counter
+        del word_doc_counter
+
+        entities = []
+        for entity, count in entity_counter.items():
+            if count < min_entity_count:
+                continue
+
+            if not category and is_category_title(entity, dump_db.language):
+                continue
+
+            if not disambi and dump_db.is_disambiguation(entity):
+                continue
+
+            entities.append(entity)
+
+        entity_dict = Trie(entities)
+        entity_stats = np.zeros((len(entity_dict), 2), dtype=np.int32)
+        for entity, index in entity_dict.items():
+            entity_stats[index][0] = entity_counter[entity]
+            entity_stats[index][1] = entity_doc_counter[entity]
+
+        del entity_counter
+        del entity_doc_counter
+
+        redirect_dict = RecordTrie(
+            "<I",
+            [
+                (title, (entity_dict[dest_title],))
+                for (title, dest_title) in dump_db.redirects()
+                if dest_title in entity_dict
+            ],
+        )
+
+        build_params = dict(
+            dump_db=dump_db.uuid,
+            dump_file=dump_db.dump_file,
+            min_word_count=min_word_count,
+            min_entity_count=min_entity_count,
+            category=category,
+            disambi=disambi,
+            build_time=time.time() - start_time,
+        )
+
+        uuid = str(uuid1().hex)
+
+        logger.info(f"{len(word_dict)} words and {len(entity_dict)} entities are indexed in the dictionary")
+
+        return Dictionary(
+            word_dict,
+            entity_dict,
+            redirect_dict,
+            word_stats,
+            entity_stats,
+            dump_db.language,
+            lowercase,
+            build_params,
+            min_paragraph_len,
+            uuid,
+        )
+
+    def save(self, out_file: str):
+        joblib.dump(self.serialize(), out_file)
+
+    def serialize(self, shared_array: bool = False) -> dict:
+        if shared_array:
+            word_stats_src = np.asarray(self._word_stats, dtype=np.int32).flatten()
+            entity_stats_src = np.asarray(self._entity_stats, dtype=np.int32).flatten()
+
+            word_stats = multiprocessing.RawArray("i", word_stats_src.size)
+            entity_stats = multiprocessing.RawArray("i", entity_stats_src.size)
+            word_stats[:] = word_stats_src
+            entity_stats[:] = entity_stats_src
+
+        else:
+            word_stats = np.asarray(self._word_stats, dtype=np.int32)
+            entity_stats = np.asarray(self._entity_stats, dtype=np.int32)
+
+        return dict(
+            word_dict=self._word_dict.tobytes(),
+            entity_dict=self._entity_dict.tobytes(),
+            redirect_dict=self._redirect_dict.tobytes(),
+            word_stats=word_stats,
+            entity_stats=entity_stats,
+            meta=dict(
+                uuid=self.uuid,
+                language=self.language,
+                lowercase=self.lowercase,
+                min_paragraph_len=self.min_paragraph_len,
+                build_params=self.build_params,
+            ),
+        )
+
+    @staticmethod
+    def load(target: Union[str, dict], mmap: bool = True) -> "Dictionary":
+        word_dict = Trie()
+        entity_dict = Trie()
+        redirect_dict = RecordTrie("<I")
+
+        if not isinstance(target, dict):
+            if mmap:
+                target = joblib.load(target, mmap_mode="r")
+            else:
+                target = joblib.load(target)
+
+        word_dict.frombytes(target["word_dict"])
+        entity_dict.frombytes(target["entity_dict"])
+        redirect_dict.frombytes(target["redirect_dict"])
+
+        word_stats = target["word_stats"]
+        entity_stats = target["entity_stats"]
+        if not isinstance(word_stats, np.ndarray):
+            word_stats = np.frombuffer(word_stats, dtype=np.int32).reshape(-1, 2)
+            entity_stats = np.frombuffer(entity_stats, dtype=np.int32).reshape(-1, 2)
+
+        return Dictionary(word_dict, entity_dict, redirect_dict, word_stats, entity_stats, **target["meta"])
+
+
+_dump_db: Optional[DumpDB] = None
+_tokenizer: Optional[BaseTokenizer] = None
+
+
+def init_worker(dump_db: DumpDB, tokenizer: BaseTokenizer):
+    global _dump_db, _tokenizer
+
+    _dump_db = dump_db
+    _tokenizer = tokenizer
+
+
+def _process_page(title: str, lowercase: bool, min_paragraph_len: int) -> tuple[Counter, Counter]:
+    word_counter = Counter()
+    entity_counter = Counter()
+
+    for paragraph in _dump_db.get_paragraphs(title):
+        entity_counter.update(link.title for link in paragraph.wiki_links)
+
+        tokens = _tokenizer.tokenize(paragraph.text)
+        if len(tokens) >= min_paragraph_len:
+            if lowercase:
+                word_counter.update(token.text.lower() for token in tokens)
+            else:
+                word_counter.update(token.text for token in tokens)
+
+    return (word_counter, entity_counter)
