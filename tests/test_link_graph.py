3875b19f29cbe124499f3ced6de923827ed557a3
Ikuya Yamada <ikuya@ikuya.net>
2024-01-02 13:43:37 +0900

    refactor link_graph.py

--- tests/test_link_graph.py
+++ tests/test_link_graph.py
@@ -1,64 +1,98 @@
-# -*- coding: utf-8 -*-
-# License: Apache License 2.0
-
-from __future__ import unicode_literals
-import numpy as np
-import pickle
-import six
+import pkg_resources
 import unittest
 from tempfile import NamedTemporaryFile
 
+import numpy as np
+
 from wikipedia2vec.dictionary import Dictionary, Entity
+from wikipedia2vec.dump_db import DumpDB
 from wikipedia2vec.link_graph import LinkGraph
 from wikipedia2vec.utils.tokenizer import get_default_tokenizer
+from wikipedia2vec.utils.wiki_dump_reader import WikiDumpReader
 
-from . import get_dump_db
-
-from nose.tools import *
+db = None
+db_file = None
+dictionary = None
+link_graph = None
 
 
 class TestLinkGraph(unittest.TestCase):
-    def setUp(self):
-        tokenizer = get_default_tokenizer('en')
-        self.dictionary = Dictionary.build(get_dump_db(), tokenizer=tokenizer, lowercase=True,
-                                           min_word_count=2, min_entity_count=1, pool_size=1,
-                                           chunk_size=1, min_paragraph_len=5, category=True,
-                                           disambi=False, progressbar=False)
-        self.link_graph = LinkGraph.build(get_dump_db(), self.dictionary, pool_size=1, chunk_size=1,
-                                          progressbar=False)
+    @classmethod
+    def setUpClass(cls):
+        global db, db_file, dictionary, link_graph
+
+        dump_file = pkg_resources.resource_filename("tests", "test_data/enwiki-pages-articles-sample.xml.bz2")
+        dump_reader = WikiDumpReader(dump_file)
+        db_file = NamedTemporaryFile()
+
+        DumpDB.build(dump_reader, db_file.name, 1, 1)
+        db = DumpDB(db_file.name)
+
+        tokenizer = get_default_tokenizer("en")
+        dictionary = Dictionary.build(
+            db,
+            tokenizer=tokenizer,
+            lowercase=True,
+            min_word_count=2,
+            min_entity_count=1,
+            min_paragraph_len=5,
+            category=False,
+            disambi=False,
+            pool_size=1,
+            chunk_size=1,
+            progressbar=False,
+        )
+        link_graph = LinkGraph.build(db, dictionary, pool_size=1, chunk_size=1, progressbar=False)
+
+    @classmethod
+    def tearDownClass(cls):
+        db_file.close()
 
     def test_uuid_property(self):
-        ok_(isinstance(self.link_graph.uuid, six.text_type))
-        eq_(32, len(self.link_graph.uuid))
+        self.assertIsInstance(link_graph.uuid, str)
+        self.assertEqual(32, len(link_graph.uuid))
 
     def test_build_params_property(self):
-        build_params = self.dictionary.build_params
-        eq_(build_params['dump_db'], get_dump_db().uuid)
-        ok_(build_params['dump_file'].endswith('enwiki-pages-articles-sample.xml.bz2'))
-        ok_(isinstance(build_params['build_time'], float))
-        ok_(build_params['build_time'] > 0)
+        build_params = link_graph.build_params
+        self.assertEqual(build_params["dump_db"], db.uuid)
+        self.assertTrue(build_params["dump_file"].endswith("enwiki-pages-articles-sample.xml.bz2"))
+        self.assertIsInstance(build_params["build_time"], float)
+        self.assertGreater(build_params["build_time"], 0)
 
     def test_neighbors(self):
-        entity = self.dictionary.get_entity('Computer accessibility')
-        neighbors = self.link_graph.neighbors(entity)
-        eq_(115, len(neighbors))
-        ok_(all(isinstance(entity, Entity) for entity in neighbors))
+        entity = dictionary.get_entity("Computer accessibility")
+        neighbors = link_graph.neighbors(entity)
+        self.assertEqual(113, len(neighbors))
+        for entity in neighbors:
+            self.assertIsInstance(entity, Entity)
 
-        titles = [e.title for e in neighbors]
-        ok_('Accessibility' in titles)
+        self.assertIn("Accessibility", [e.title for e in neighbors])
+
+    def test_neighbor_indices(self):
+        index = dictionary.get_entity_index("Computer accessibility")
+        neighbor_indices = link_graph.neighbor_indices(index)
+        self.assertIsInstance(neighbor_indices, np.ndarray)
+        self.assertEqual(113, neighbor_indices.size)
+
+        self.assertIn(dictionary.get_entity_index("Accessibility"), neighbor_indices)
 
     def test_save_load(self):
         def validate(obj):
-            s1 = self.link_graph.serialize()
+            s1 = link_graph.serialize()
             s2 = obj.serialize()
             for key in s1.keys():
                 if isinstance(s1[key], np.ndarray):
                     np.array_equal(s1[key], s2[key])
                 else:
-                    eq_(s1[key], s2[key])
+                    self.assertEqual(s1[key], s2[key])
 
-        validate(LinkGraph.load(self.link_graph.serialize(), self.dictionary))
+        validate(LinkGraph.load(link_graph.serialize(), dictionary))
+        validate(LinkGraph.load(link_graph.serialize(shared_array=True), dictionary))
 
         with NamedTemporaryFile() as f:
-            self.link_graph.save(f.name)
-            validate(LinkGraph.load(f.name, self.dictionary))
+            link_graph.save(f.name)
+            validate(LinkGraph.load(f.name, dictionary))
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ wikipedia2vec/link_graph.py
@@ -0,0 +1,155 @@
+import logging
+import multiprocessing
+import time
+from contextlib import closing
+from functools import partial
+from itertools import chain
+from multiprocessing.pool import Pool
+from typing import Optional, List, Tuple, Union
+from uuid import uuid1
+
+import joblib
+import numpy as np
+from scipy.sparse import coo_matrix
+from tqdm import tqdm
+
+from .dictionary import Dictionary, Entity
+from .dump_db import DumpDB
+
+logger = logging.getLogger(__name__)
+
+
+class LinkGraph:
+    def __init__(
+        self, dictionary: Dictionary, indices: np.ndarray, indptr: np.ndarray, build_params: dict, uuid: str = ""
+    ):
+        self.uuid = uuid
+        self.build_params = build_params
+        self._dictionary = dictionary
+        self._indptr = indptr
+        self._indices = indices
+
+        self._offset = self._dictionary.entity_offset
+
+    def neighbors(self, item: Entity) -> List[Entity]:
+        return [self._dictionary.get_entity_by_index(i) for i in self.neighbor_indices(item.index).tolist()]
+
+    def neighbor_indices(self, index: int) -> np.ndarray:
+        index -= self._offset
+        return self._indices[self._indptr[index] : self._indptr[index + 1]]
+
+    def serialize(self, shared_array: bool = False) -> dict:
+        if shared_array:
+            indices = multiprocessing.RawArray("i", len(self._indices))
+            indptr = multiprocessing.RawArray("i", len(self._indptr))
+
+            indices[:] = self._indices
+            indptr[:] = self._indptr
+        else:
+            indices = np.asarray(self._indices, dtype=np.int32)
+            indptr = np.asarray(self._indptr, dtype=np.int32)
+
+        return dict(indices=indices, indptr=indptr, build_params=self.build_params, uuid=self.uuid)
+
+    def save(self, out_file: str):
+        joblib.dump(self.serialize(), out_file)
+
+    @staticmethod
+    def load(target: Union[str, dict], dictionary: Dictionary, mmap: bool = True) -> "LinkGraph":
+        if not isinstance(target, dict):
+            if mmap:
+                target = joblib.load(target, mmap_mode="r")
+            else:
+                target = joblib.load(target)
+
+        if target["build_params"]["dictionary"] != dictionary.uuid:
+            raise RuntimeError("The specified dictionary is different from the one used to build this link graph")
+
+        indices = target.pop("indices")
+        indptr = target.pop("indptr")
+        if not isinstance(indices, np.ndarray):
+            indices = np.frombuffer(indices, dtype=np.int32)
+            indptr = np.frombuffer(indptr, dtype=np.int32)
+
+        return LinkGraph(dictionary, indices, indptr, **target)
+
+    @staticmethod
+    def build(
+        dump_db: DumpDB, dictionary: Dictionary, pool_size: int, chunk_size: int, progressbar: bool = True
+    ) -> "LinkGraph":
+        start_time = time.time()
+
+        logger.info("Step 1/2: Processing Wikipedia pages...")
+
+        with closing(
+            Pool(pool_size, initializer=init_worker, initargs=(dump_db, dictionary.serialize(shared_array=True)))
+        ) as pool:
+            rows = []
+            cols = []
+
+            with tqdm(total=dump_db.page_size(), mininterval=0.5, disable=not progressbar) as bar:
+                f = partial(_process_page, offset=dictionary.entity_offset)
+
+                for ret in pool.imap_unordered(f, dump_db.titles(), chunksize=chunk_size):
+                    if ret:
+                        page_indices, dest_indices = ret
+                        rows.append(page_indices)
+                        rows.append(dest_indices)
+                        cols.append(dest_indices)
+                        cols.append(page_indices)
+
+                    bar.update(1)
+
+        logger.info("Step 2/2: Converting matrix...")
+
+        rows = list(chain(*rows))
+        cols = list(chain(*cols))
+
+        matrix = coo_matrix(
+            (np.ones(len(rows), dtype=bool), (np.array(rows, dtype=np.int32), np.array(cols, dtype=np.int32))),
+            dtype=bool,
+        )
+        del rows, cols
+
+        matrix = matrix.tocsr()
+
+        matrix.indices += dictionary.entity_offset
+
+        build_params = dict(
+            dump_file=dump_db.dump_file,
+            dump_db=dump_db.uuid,
+            dictionary=dictionary.uuid,
+            build_time=time.time() - start_time,
+        )
+
+        uuid = str(uuid1().hex)
+
+        return LinkGraph(dictionary, matrix.indices, matrix.indptr, build_params, uuid)
+
+
+_dump_db: Optional[DumpDB] = None
+_dictionary: Optional[Dictionary] = None
+
+
+def init_worker(dump_db: DumpDB, dictionary_obj: dict):
+    global _dump_db, _dictionary
+
+    _dump_db = dump_db
+    _dictionary = Dictionary.load(dictionary_obj)
+
+
+def _process_page(title: str, offset: int) -> Optional[Tuple[List[int], List[int]]]:
+    page_index = _dictionary.get_entity_index(title)
+    if page_index == -1:
+        return None
+
+    page_index -= offset
+
+    dest_indices = []
+    for paragraph in _dump_db.get_paragraphs(title):
+        for link in paragraph.wiki_links:
+            dest_index = _dictionary.get_entity_index(link.title)
+            if dest_index != -1:
+                dest_indices.append(dest_index - offset)
+
+    return ([page_index] * len(dest_indices), dest_indices)
