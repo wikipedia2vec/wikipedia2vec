ae163f3ec72579fe12c88a15983d6722125db38c
Ikuya Yamada <ikuya@ikuya.net>
2024-01-02 17:11:53 +0900

    refactor mention_db.py and add tests

---
+++ tests/test_mention_db.py
@@ -0,0 +1,156 @@
+import pkg_resources
+import unittest
+from tempfile import NamedTemporaryFile
+
+from wikipedia2vec.dictionary import Dictionary
+from wikipedia2vec.dump_db import DumpDB
+from wikipedia2vec.mention_db import Mention, MentionDB
+from wikipedia2vec.utils.tokenizer import get_default_tokenizer
+from wikipedia2vec.utils.wiki_dump_reader import WikiDumpReader
+
+db = None
+db_file = None
+tokenizer = None
+dictionary = None
+
+
+def setUpModule():
+    global db, db_file, tokenizer, dictionary
+
+    dump_file = pkg_resources.resource_filename("tests", "test_data/enwiki-pages-articles-sample.xml.bz2")
+    dump_reader = WikiDumpReader(dump_file)
+    db_file = NamedTemporaryFile()
+
+    DumpDB.build(dump_reader, db_file.name, 1, 1)
+    db = DumpDB(db_file.name)
+
+    tokenizer = get_default_tokenizer("en")
+    dictionary = Dictionary.build(
+        db,
+        tokenizer=tokenizer,
+        lowercase=True,
+        min_word_count=2,
+        min_entity_count=1,
+        min_paragraph_len=5,
+        category=False,
+        disambi=False,
+        pool_size=1,
+        chunk_size=1,
+        progressbar=False,
+    )
+
+
+def tearDownModule():
+    db_file.close()
+
+
+class TestMention(unittest.TestCase):
+    def setUp(self):
+        self._entity = dictionary.get_entity("Computer accessibility")
+        self._mention = Mention(
+            dictionary,
+            text="mention text",
+            index=self._entity.index,
+            link_count=5,
+            total_link_count=10,
+            doc_count=100,
+            start=3,
+            end=6,
+        )
+
+    def test_text_property(self):
+        self.assertEqual("mention text", self._mention.text)
+
+    def test_index_property(self):
+        self.assertEqual(dictionary.get_entity("Computer accessibility").index, self._mention.index)
+
+    def test_count_properties(self):
+        self.assertEqual(5, self._mention.link_count)
+        self.assertEqual(10, self._mention.total_link_count)
+        self.assertEqual(100, self._mention.doc_count)
+
+    def test_start_and_end_properies(self):
+        self.assertEqual(3, self._mention.start)
+        self.assertEqual(6, self._mention.end)
+
+    def test_entity_property(self):
+        self.assertEqual("Computer accessibility", self._mention.entity.title)
+
+    def test_link_prob_property(self):
+        self.assertAlmostEqual(0.1, self._mention.link_prob)
+
+    def test_prior_prob_property(self):
+        self.assertAlmostEqual(0.5, self._mention.prior_prob)
+
+
+mention_db = None
+
+
+class TestMentionDB(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        global mention_db
+        mention_db = MentionDB.build(
+            dump_db=db,
+            dictionary=dictionary,
+            tokenizer=tokenizer,
+            min_link_prob=0.01,
+            min_prior_prob=0.01,
+            max_mention_len=5,
+            case_sensitive=False,
+            pool_size=1,
+            chunk_size=1,
+            progressbar=False,
+        )
+
+    def test_iterator(self):
+        for mention in mention_db:
+            self.assertIsInstance(mention, Mention)
+            # print(mention.entity.title, mention.text)
+
+    def test_query(self):
+        mentions = mention_db.query("web")
+        self.assertEqual(1, len(mentions))
+        self.assertEqual(mentions[0].text, "web")
+        self.assertEqual(mentions[0].entity.title, "World Wide Web")
+
+    def test_prefix_search(self):
+        self.assertEqual(mention_db.prefix_search("web service", start=0), ["web"])
+        self.assertEqual(mention_db.prefix_search("this is a web service", start=0), [])
+        self.assertEqual(mention_db.prefix_search("this is a web service", start=10), ["web"])
+
+    def test_detect_mentions(self):
+        text = "this is a web service"
+        tokens = tokenizer.tokenize(text)
+        mentions = mention_db.detect_mentions(text, tokens)
+        self.assertEqual(1, len(mentions))
+        self.assertEqual(mentions[0].text, "web")
+        self.assertEqual(mentions[0].entity.title, "World Wide Web")
+
+    def test_uuid_property(self):
+        self.assertIsInstance(mention_db.uuid, str)
+        self.assertEqual(32, len(mention_db.uuid))
+
+    def test_build_params_property(self):
+        build_params = mention_db.build_params
+        self.assertEqual(build_params["dump_db"], db.uuid)
+        self.assertTrue(build_params["dump_file"].endswith("enwiki-pages-articles-sample.xml.bz2"))
+        self.assertIsInstance(build_params["build_time"], float)
+        self.assertGreater(build_params["build_time"], 0)
+
+    def test_save_load(self):
+        def validate(obj):
+            s1 = mention_db.serialize()
+            s2 = obj.serialize()
+            for key in s1.keys():
+                self.assertEqual(s1[key], s2[key])
+
+        validate(MentionDB.load(mention_db.serialize(), dictionary))
+
+        with NamedTemporaryFile() as f:
+            mention_db.save(f.name)
+            validate(MentionDB.load(f.name, dictionary))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- wikipedia2vec/mention_db.py
+++ wikipedia2vec/mention_db.py
@@ -1,30 +1,48 @@
-# -*- coding: utf-8 -*-
-# cython: profile=False
-# License: Apache License 2.0
-
-import joblib
 import logging
 import pkg_resources
-import six
-import re
 import time
 from collections import defaultdict, Counter
 from contextlib import closing
 from functools import partial
-from marisa_trie import Trie, RecordTrie
 from multiprocessing.pool import Pool
-from tqdm import tqdm
+from typing import FrozenSet, Iterator, List, Optional, Tuple, Union
 from uuid import uuid1
 
-from .dump_db cimport DumpDB, Paragraph, WikiLink
-from .utils.tokenizer.token cimport Token
+import cython
+import joblib
+from marisa_trie import Trie, RecordTrie
+from tqdm import tqdm
+
+from .dictionary import Dictionary, Entity
+from .dump_db import DumpDB
+from .utils.tokenizer.base_tokenizer import BaseTokenizer
+from .utils.tokenizer.token import Token
 
 logger = logging.getLogger(__name__)
 
 
-cdef class Mention:
-    def __init__(self, Dictionary dictionary, unicode text, int32_t index, int32_t link_count,
-                 int32_t total_link_count, int32_t doc_count, int32_t start=-1, int32_t end=-1):
+@cython.cclass
+class Mention:
+    text = cython.declare(str, visibility="readonly")
+    index = cython.declare(int, visibility="readonly")
+    link_count = cython.declare(int, visibility="readonly")
+    total_link_count = cython.declare(int, visibility="readonly")
+    doc_count = cython.declare(int, visibility="readonly")
+    start = cython.declare(int, visibility="readonly")
+    end = cython.declare(int, visibility="readonly")
+    _dictionary: cython.declare(Dictionary)
+
+    def __init__(
+        self,
+        dictionary: Dictionary,
+        text: str,
+        index: int,
+        link_count: int,
+        total_link_count: int,
+        doc_count: int,
+        start: int = -1,
+        end: int = -1,
+    ):
         self.text = text
         self.index = index
         self.link_count = link_count
@@ -36,37 +54,42 @@ cdef class Mention:
         self._dictionary = dictionary
 
     @property
-    def entity(self):
+    def entity(self) -> Entity:
         return self._dictionary.get_entity_by_index(self.index)
 
     @property
-    def link_prob(self):
+    def link_prob(self) -> float:
         if self.doc_count > 0:
             return min(1.0, float(self.total_link_count) / self.doc_count)
         else:
             return 0.0
 
     @property
-    def prior_prob(self):
+    def prior_prob(self) -> float:
         if self.total_link_count > 0:
             return min(1.0, float(self.link_count) / self.total_link_count)
         else:
             return 0.0
 
     @property
-    def commonness(self):
+    def commonness(self) -> float:
         return self.prior_prob
 
     def __repr__(self):
-        if six.PY2:
-            return b'<Mention %s -> %s>' % (self.text.encode('utf-8'), self.entity.title.encode('utf-8'))
-        else:
-            return '<Mention %s -> %s>' % (self.text, self.entity.title)
+        return f"<Mention {self.text}->{self.entity.title}>"
 
 
-cdef class MentionDB(object):
-    def __init__(self, mention_trie, data_trie, dictionary, bint case_sensitive,
-                 int32_t max_mention_len, dict build_params, unicode uuid):
+class MentionDB:
+    def __init__(
+        self,
+        mention_trie: Trie,
+        data_trie: RecordTrie,
+        dictionary: Dictionary,
+        case_sensitive: bool,
+        max_mention_len: int,
+        build_params: dict,
+        uuid: str,
+    ):
         self.mention_trie = mention_trie
         self.data_trie = data_trie
         self.uuid = uuid
@@ -76,37 +99,26 @@ cdef class MentionDB(object):
         self._case_sensitive = case_sensitive
         self._max_mention_len = max_mention_len
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Mention]:
         for text in self.mention_trie:
             for obj in self.data_trie[text]:
                 yield Mention(self._dictionary, text, *obj)
 
-    cpdef list query(self, unicode text):
+    def query(self, text: str) -> List[Mention]:
         if not self._case_sensitive:
             text = text.lower()
 
         return [Mention(self._dictionary, text, *o) for o in self.data_trie[text]]
 
-    cpdef list prefix_search(self, unicode text, int32_t start=0):
+    def prefix_search(self, text: str, start: int = 0) -> List[str]:
         if not self._case_sensitive:
             text = text.lower()
 
-        return self._prefix_search(text, start)
-
-    cdef inline list _prefix_search(self, unicode text, int32_t start=0):
-        cdef list ret = self.mention_trie.prefixes(text[start:start+self._max_mention_len])
+        ret = self.mention_trie.prefixes(text[start : start + self._max_mention_len])
         ret.sort(key=len, reverse=True)
         return ret
 
-    cpdef list detect_mentions(self, unicode text, list tokens, set entity_indices_in_page=set()):
-        cdef int32_t cur, start, end, index
-        cdef unicode prefix, target_text
-        cdef tuple c
-        cdef list ret, candidates, matched
-        cdef frozenset end_offsets
-        cdef Mention mention
-        cdef Token token
-
+    def detect_mentions(self, text: str, tokens: List[Token], entity_indices_in_page: set = set()) -> List[Mention]:
         end_offsets = frozenset([token.end for token in tokens])
 
         ret = []
@@ -121,62 +133,68 @@ cdef class MentionDB(object):
             if cur > start:
                 continue
 
-            for prefix in self._prefix_search(target_text, start=start):
+            for prefix in self.prefix_search(target_text, start=start):
                 end = start + len(prefix)
                 if end in end_offsets:
                     cur = end
                     candidates = self.data_trie[prefix]
                     if len(candidates) == 1:
-                        ret.append(Mention(self._dictionary, text[start:end], *candidates[0],
-                                           start=start, end=end))
+                        ret.append(Mention(self._dictionary, text[start:end], *candidates[0], start=start, end=end))
                     else:
                         matched = [c for c in candidates if c[0] in entity_indices_in_page]
                         if len(matched) == 1:
                             (index, *args) = matched[0]
-                            ret.append(Mention(self._dictionary, text[start:end], index, *args,
-                                               start=start, end=end))
+                            ret.append(Mention(self._dictionary, text[start:end], index, *args, start=start, end=end))
                     break
 
         return ret
 
     @staticmethod
-    def build(dump_db, dictionary, tokenizer, min_link_prob, min_prior_prob, max_mention_len,
-              case_sensitive, pool_size, chunk_size, progressbar=True):
+    def build(
+        dump_db: DumpDB,
+        dictionary: Dictionary,
+        tokenizer: BaseTokenizer,
+        min_link_prob: float,
+        min_prior_prob: float,
+        max_mention_len: int,
+        case_sensitive: bool,
+        pool_size: int,
+        chunk_size: int,
+        progressbar: bool = True,
+    ) -> "MentionDB":
         start_time = time.time()
 
-        logger.info('Step 1/3: Starting to iterate over Wikipedia pages...')
+        logger.info("Step 1/3: Starting to iterate over Wikipedia pages...")
 
         name_dict = defaultdict(lambda: Counter())
         init_args = [dump_db, dictionary.serialize(shared_array=True), tokenizer, None]
 
         with closing(Pool(pool_size, initializer=init_worker, initargs=init_args)) as pool:
             with tqdm(total=dump_db.page_size(), mininterval=0.5, disable=not progressbar) as bar:
-                f = partial(_extract_links, max_mention_len=max_mention_len,
-                            case_sensitive=case_sensitive)
+                f = partial(_extract_links, max_mention_len=max_mention_len, case_sensitive=case_sensitive)
                 for ret in pool.imap_unordered(f, dump_db.titles(), chunksize=chunk_size):
-                    for (text, index) in ret:
+                    for text, index in ret:
                         name_dict[text][index] += 1
                     bar.update(1)
 
-        logger.info('Step 2/3: Starting to count occurrences...')
+        logger.info("Step 2/3: Starting to count occurrences...")
 
-        name_trie = Trie(six.iterkeys(name_dict))
+        name_trie = Trie(name_dict.keys())
 
         name_counter = Counter()
         init_args[3] = name_trie
 
         with closing(Pool(pool_size, initializer=init_worker, initargs=init_args)) as pool:
             with tqdm(total=dump_db.page_size(), mininterval=0.5, disable=not progressbar) as bar:
-                f = partial(_count_occurrences, max_mention_len=max_mention_len,
-                            case_sensitive=case_sensitive)
+                f = partial(_count_occurrences, max_mention_len=max_mention_len, case_sensitive=case_sensitive)
                 for names in pool.imap_unordered(f, dump_db.titles(), chunksize=chunk_size):
                     name_counter.update(names)
                     bar.update(1)
 
-        logger.info('Step 3/3: Building DB...')
+        logger.info("Step 3/3: Building DB...")
 
         def item_generator():
-            for (name, entity_counter) in six.iteritems(name_dict):
+            for name, entity_counter in name_dict.items():
                 doc_count = name_counter[name]
                 total_link_count = sum(entity_counter.values())
 
@@ -187,60 +205,66 @@ cdef class MentionDB(object):
                 if link_prob < min_link_prob:
                     continue
 
-                for (index, link_count) in six.iteritems(entity_counter):
+                for index, link_count in entity_counter.items():
                     prior_prob = float(link_count) / total_link_count
                     if prior_prob < min_prior_prob:
                         continue
 
                     yield (name, (index, link_count, total_link_count, doc_count))
 
-        data_trie = RecordTrie('<IIII', item_generator())
+        data_trie = RecordTrie("<IIII", item_generator())
         mention_trie = Trie(data_trie.keys())
 
-        uuid = six.text_type(uuid1().hex)
+        uuid = str(uuid1().hex)
 
-        build_params = dict(dump_file=dump_db.dump_file,
-                            dump_db=dump_db.uuid,
-                            dictionary=dictionary.uuid,
-                            build_time=time.time() - start_time,
-                            version=pkg_resources.get_distribution('wikipedia2vec').version)
+        build_params = dict(
+            dump_file=dump_db.dump_file,
+            dump_db=dump_db.uuid,
+            dictionary=dictionary.uuid,
+            build_time=time.time() - start_time,
+            version=pkg_resources.get_distribution("wikipedia2vec").version,
+        )
 
-        return MentionDB(mention_trie, data_trie, dictionary, case_sensitive, max_mention_len,
-                         build_params, uuid)
+        return MentionDB(mention_trie, data_trie, dictionary, case_sensitive, max_mention_len, build_params, uuid)
 
-    def serialize(self):
-        return dict(mention_trie=self.mention_trie.tobytes(),
-                    data_trie=self.data_trie.tobytes(),
-                    kwargs=dict(max_mention_len=self._max_mention_len,
-                                case_sensitive=self._case_sensitive,
-                                build_params=self.build_params, uuid=self.uuid))
+    def serialize(self) -> dict:
+        return dict(
+            mention_trie=self.mention_trie.tobytes(),
+            data_trie=self.data_trie.tobytes(),
+            kwargs=dict(
+                max_mention_len=self._max_mention_len,
+                case_sensitive=self._case_sensitive,
+                build_params=self.build_params,
+                uuid=self.uuid,
+            ),
+        )
 
-    def save(self, out_file):
+    def save(self, out_file: str):
         joblib.dump(self.serialize(), out_file)
 
     @staticmethod
-    def load(target, dictionary):
+    def load(target: Union[str, dict], dictionary: Dictionary) -> "MentionDB":
         if not isinstance(target, dict):
             target = joblib.load(target)
 
-        if target['kwargs']['build_params']['dictionary'] != dictionary.uuid:
-            raise RuntimeError('The specified dictionary is different from the one used to build this DB')
+        if target["kwargs"]["build_params"]["dictionary"] != dictionary.uuid:
+            raise RuntimeError("The specified dictionary is different from the one used to build this DB")
 
         mention_trie = Trie()
-        mention_trie = mention_trie.frombytes(target['mention_trie'])
-        data_trie = RecordTrie('<IIII')
-        data_trie = data_trie.frombytes(target['data_trie'])
+        mention_trie = mention_trie.frombytes(target["mention_trie"])
+        data_trie = RecordTrie("<IIII")
+        data_trie = data_trie.frombytes(target["data_trie"])
 
-        return MentionDB(mention_trie, data_trie, dictionary, **target['kwargs'])
-
-
-cdef Dictionary _dictionary = None
-cdef DumpDB _dump_db = None
-cdef _tokenizer = None
-cdef _name_trie = None
+        return MentionDB(mention_trie, data_trie, dictionary, **target["kwargs"])
 
 
-def init_worker(dump_db, dictionary_obj, tokenizer, name_trie=None):
+_dictionary: Optional[Dictionary] = None
+_dump_db: Optional[DumpDB] = None
+_tokenizer: Optional[BaseTokenizer] = None
+_name_trie: Optional[Trie] = None
+
+
+def init_worker(dump_db: DumpDB, dictionary_obj: dict, tokenizer: BaseTokenizer, name_trie: Trie = None):
     global _dump_db, _dictionary, _tokenizer, _name_trie
 
     _dump_db = dump_db
@@ -249,12 +273,7 @@ def init_worker(dump_db, dictionary_obj, tokenizer, name_trie=None):
     _name_trie = name_trie
 
 
-def _extract_links(unicode title, int32_t max_mention_len, bint case_sensitive):
-    cdef unicode text
-    cdef list ret
-    cdef WikiLink wiki_link
-    cdef Paragraph paragraph
-
+def _extract_links(title: str, max_mention_len: int, case_sensitive: bool) -> List[Tuple[str, int]]:
     ret = []
 
     for paragraph in _dump_db.get_paragraphs(title):
@@ -274,14 +293,7 @@ def _extract_links(unicode title, int32_t max_mention_len, bint case_sensitive):
     return ret
 
 
-def _count_occurrences(unicode title, int32_t max_mention_len, bint case_sensitive):
-    cdef int32_t start
-    cdef unicode text, prefix
-    cdef list ret
-    cdef frozenset end_offsets
-    cdef Token token
-    cdef Paragraph paragraph
-
+def _count_occurrences(title: str, max_mention_len: int, case_sensitive: bool) -> FrozenSet[str]:
     ret = []
 
     for paragraph in _dump_db.get_paragraphs(title):
@@ -295,7 +307,7 @@ def _count_occurrences(unicode title, int32_t max_mention_len, bint case_sensiti
 
         for token in tokens:
             start = token.start
-            for prefix in _name_trie.prefixes(text[start:start+max_mention_len]):
+            for prefix in _name_trie.prefixes(text[start : start + max_mention_len]):
                 if (start + len(prefix)) in end_offsets:
                     ret.append(prefix)
 
