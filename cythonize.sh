4552866dd9dd28fcb9b995303b4a85bd0ccd4454
Ikuya Yamada <ikuya@ikuya.net>
2024-01-06 17:19:08 +0900

    update cythonize.sh

--- cythonize.sh
+++ cythonize.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cython --cplus wikipedia2vec/*.pyx
-cython --cplus wikipedia2vec/utils/*.pyx
-cython --cplus wikipedia2vec/utils/tokenizer/*.pyx
-cython --cplus wikipedia2vec/utils/sentence_detector/*.pyx
+cython --cplus -3 wikipedia2vec/dictionary.py
+cython --cplus -3 wikipedia2vec/dump_db.py
+cython --cplus -3 wikipedia2vec/link_graph.py
+cython --cplus -3 wikipedia2vec/mention_db.py
+cython --cplus -3 wikipedia2vec/wikipedia2vec.py
+cython --cplus -3 wikipedia2vec/utils/sentence_detector/sentence.py
+cython --cplus -3 wikipedia2vec/utils/tokenizer/token.py
+cython --cplus -3 wikipedia2vec/utils/wiki_page.py
